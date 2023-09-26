881a5e6029ccef32577c2dd9c212b2d1f6afaaa0
Almog <almogluzon@gmail.com>
2023-09-26 19:14:55 +0300

    base setup. server and empty files

---
+++ server.js
@@ -0,0 +1,21 @@
+// npm i express mysql2 ejs body-parser
+const express = require('express');
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.set("view engine", "ejs");
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, "css")));
+app.use(express.static(path.join(__dirname, "js")));
+
+var db_M = require('./database');
+global.db_pool = db_M.pool;
+
+app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
+    console.log(`Now listening on port http://localhost:${port}`);
+});
